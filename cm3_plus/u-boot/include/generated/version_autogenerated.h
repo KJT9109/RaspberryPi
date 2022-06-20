51e10f5f4d32bfe24f7ba439c440d27bd7af382c
jitae.kim <kjt9109@naver.com>
2022-06-20 05:25:09 +0000

    U-boot Config & Directory     ------------------------------------------------------------------     main:     파일 접근 오류로 재 업로드     cm3용 configs/rpi_arm64_cm3_defconfig 추가

---
+++ cm3_plus/u-boot/include/generated/version_autogenerated.h
@@ -0,0 +1,6 @@
+#define PLAIN_VERSION "2022.07-rc4-00019-gc18e5fb055"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define U_BOOT_VERSION_NUM 2022
+#define U_BOOT_VERSION_NUM_PATCH 7
+#define CC_VERSION_STRING "aarch64-linux-gnu-gcc (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils for Ubuntu) 2.34"
